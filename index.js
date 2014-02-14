4b7455405db76c1cb9e9abd99380daed5c469929
Andrew Shaffer <andrew@weo.io>
2014-02-14 11:44:35 -0800

    add deps

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 var through = require('through')
   , syntaxError = require('syntax-error')
-  , _ = require('underscore')
+  , _ = require('lodash')
   , fs = require('fs')
-  , errors = []
-  , errFile = 'lib/error/err.msg';
+  , errors = [];
 
-module.exports = {
-  transform: function(file) {
-    if(_.last(file.split('.')) === 'js') {
-      var err = syntaxError(fs.readFileSync(file), file);
-      if(err) {
-        if(! _.contains(errors, file))
-          errors.push(file);
+module.exports = function(errFile) {
+  return {
+    transform: function(file) {
+      if(_.last(file.split('.')) === 'js') {
+        var err = syntaxError(fs.readFileSync(file), file);
+        if(err) {
+          if(! _.contains(errors, file))
+            errors.push(file);
 
-        fs.writeFileSync(errFile, err);
-        return through(function() {
+          fs.writeFileSync(errFile, err);
+          return through(function() {
 
-        }, function() {
-          this.queue('');
-          this.queue(null);
-        });
+          }, function() {
+            this.queue('');
+            this.queue(null);
+          });
+        }
       }
-    }
 
-    errors = _.without(errors, file);
-    return through();
-  },
-  postBundleCb: function(err, src, cb) {
-    if(errors.length === 0 && fs.existsSync(errFile))
-      fs.unlinkSync(errFile);
-    cb && cb(err, src);
-  }
+      errors = _.without(errors, file);
+      return through();
+    },
+    postBundleCb: function(err, src, cb) {
+      if(errors.length === 0 && fs.existsSync(errFile))
+        fs.unlinkSync(errFile);
+      cb && cb(err, src);
+    }
+  };
 };
